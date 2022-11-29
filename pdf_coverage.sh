55abaa0c398375ac4c0c716a39d95c40b14a9733
tdr <dsm.iv.tr@gmail.com>
2022-11-29 11:34:19 -0500

    Add files via upload

---
+++ pdf_coverage.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# from somewhere on stackexchange...
+
+IN="$1"
+filename=$(basename "${IN}")
+filename="${filename%.*}"
+PAGES=$(pdfinfo "$IN" | grep ^Pages: | tr -dc '0-9')
+
+non_blank() {
+    for i in $(seq 1 $PAGES)
+    do
+        # or try "inkcov"
+        PERCENT=$(gs -o -  -dFirstPage=${i} -dLastPage=${i} -sDEVICE=ink_cov "$IN" | grep CMYK | nawk 'BEGIN { sum=0; } {sum += $1 + $2 + $3 + $4;} END { printf "%.5f\n", sum } ')
+        if [ $(echo "$PERCENT > 0.100" | bc) -eq 1 ]
+        then
+            echo $i
+            #echo $i 1>&2
+        fi
+        echo -n . 1>&2
+    done | tee "$filename.tmp"
+    echo 1>&2
+}
+
+set +x
+pdftk "${IN}" cat $(non_blank) output "${filename}.stripped.pdf"
